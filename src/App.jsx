698599a9e549ae41eacdda3f5905a4deda4d6b25
himanshuTambeGodL <tambe.himanshu@webseeder.tech>
2025-11-04 17:39:54 +0530

    Merge remote changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
+import React, { lazy, Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from './Pages/Dashboard';
 
-import React from 'react'
-import LoginPage from './pages/login/Login'
-function App() {
- 
-
-import Dashboard from "./Pages/Dashboard";
+// Lazy load the Login page
+const LoginPage = lazy(() => import('./pages/login/Login'));
 
 export default function App() {
-
   return (
-    <div>
-     <LoginPage/>
-
-      <div className="flex-1 transition-all duration-300 ease-in-out">
-        <Dashboard />
+    <>
+      <div>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </Suspense>
       </div>
-
-    </div>
+    </>
   );
 }
