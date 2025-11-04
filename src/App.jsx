d1e2293a94b6366748dcb1ef1d4519d94fd2dce6
himanshuTambeGodL <tambe.himanshu@webseeder.tech>
2025-11-04 10:27:45 +0530

    login page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import LoginPage from './pages/login/Login'
 function App() {
  
   return (
     <>
     <div>
-      Hello World
+     <LoginPage/>
     </div>
     </>
   )
