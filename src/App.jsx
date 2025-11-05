ee48c7ec2b0ec89cafe620736643d86c8eb64e44
himanshuTambeGodL <tambe.himanshu@webseeder.tech>
2025-11-05 12:03:09 +0530

    check

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,29 @@
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
-import Dashboard from "./Pages/Dashboard";
+import Dashboard from './pages/Dashboard';
 import MainLayout from "./components/layouts/MainLayout";
 
 function App() {
   return (
-    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route
+          path="/dashboard"
+          element={
+            <MainLayout>
+              <Dashboard />
+            </MainLayout>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
       <Routes>
         <Route path="/" element={<Login />} />
         <Route
