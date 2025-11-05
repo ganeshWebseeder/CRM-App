d52f88f9c850d9edfb9c400762573b8da2887a0e
ganeshborole <ganeshborole73@gmail.com>
2025-11-05 12:17:33 +0530

    fix: correct case for Login.jsx import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login.jsx";
+import Login from "./pages/Login";
 import Dashboard from "./Pages/Dashboard";
 import MainLayout from "./components/layouts/MainLayout";
 
