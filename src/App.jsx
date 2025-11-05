c983e39f475b65436f19c987df8d76a844b5694d
ganeshborole <ganeshborole73@gmail.com>
2025-11-05 12:02:59 +0530

    fix: corrected Login import path

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Pages/Login.jsx";
-
 import Dashboard from "./Pages/Dashboard";
 import MainLayout from "./components/layouts/MainLayout";
 
