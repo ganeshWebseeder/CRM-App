bebd5f45388bfe13685b330e7348c6e52871e536
ganeshborole <ganeshborole73@gmail.com>
2025-11-05 18:31:32 +0530

    vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),  tailwindcss(),],
+  plugins: [react(), tailwindcss()],
+  build: {
+    // Raise the warning limit for large JS chunks (default is 500 kB)
+    chunkSizeWarningLimit: 1000, // warns only if > 1 MB
+  },
 })
