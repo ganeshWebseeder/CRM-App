e3ece741c0841fe550b55a0a03badc53674d1fb4
ganeshborole <ganeshborole73@gmail.com>
2025-11-05 11:09:19 +0530

    add changes inside the sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
+import Dashboard from "./Pages/Dashboard";
 import MainLayout from "./components/layouts/MainLayout";
 
 function App() {
