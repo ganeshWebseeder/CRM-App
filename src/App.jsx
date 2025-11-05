6e0ea6b0e7b011b1c08601dece228bf6a976d08d
himanshuTambeGodL <tambe.himanshu@webseeder.tech>
2025-11-05 13:16:05 +0530

    error solved

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import MainLayout from "./components/layouts/MainLayout";
 
 function App() {
   return (
-    <BrowserRouter>
+
       <Routes>
         <Route path="/" element={<Login />} />
         <Route
@@ -19,7 +19,7 @@ function App() {
           }
         />
       </Routes>
-    </BrowserRouter>
+   
   );
 }
 
